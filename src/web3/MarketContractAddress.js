be296ee99bf49294f5839f2607f4f11766c30017
apple <apple@192.168.2.104>
2021-06-07 10:09:14 +0300

    fix mainnet

--- src/web3/MarketContractAddress.js
+++ src/web3/MarketContractAddress.js
@@ -4,6 +4,8 @@ let add = "";
 if (networkType === "testnet") {
   // add = "0xe84B1bd201b9886edCDc8A79e9A70c1d8D5Ea1A8"; // test net
   add = "0xc2F19E2be5c5a1AA7A998f44B759eb3360587ad1"; // test net
+} else if (networkType === "mainnet") {
+  add = "0x1c06d9295f9EBa99F51f4d0c143Baac4eC42A6b2"; // main net
 }
 
 export default add;
--- src/web3/networkType.js
+++ src/web3/networkType.js
@@ -1,1 +1,1 @@
-export const networkType = "testnet";
+export const networkType = "mainnet";
