57360e4948b9f1721253faa7cbcd34e8026fa9c3
Haris Shahid <harisshahid00@gmail.com>
2021-11-26 14:40:32 +0500

    text alignment issue resolved as platform wise

--- src/components/appHeader.js
+++ src/components/appHeader.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, View, TouchableOpacity, Image, Text } from "react-native";
+import { StyleSheet, View, TouchableOpacity, Image, Text, Platform } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 
 import Colors from '../constants/Colors';
@@ -10,6 +10,11 @@ import { FONTS, COLORS } from '../constants';
 
 function AppHeader(props) {
     const navigation = useNavigation();
+
+    let labelStyle = Platform.OS === "android" ? {
+        flex: 1,
+        textAlignVertical: "center",
+    } : {}
 
     return (
         <View style={[styles.container, props.containerStyle]} >
@@ -23,12 +28,12 @@ function AppHeader(props) {
                         </TouchableOpacity> : null
                 }
             </View>
-            <View style={{ height: "100%", justifyContent: "center"}} >
+            <View style={{ height: "100%", justifyContent: "center" }} >
                 {
                     props.titleComponent ?
                         props.titleComponent
                         :
-                        <Text style={[styles.title, { color: props.isWhite ? Colors.white : Colors.black }, props.titleStyle]} >{props.title}</Text>
+                        <Text style={[styles.title, labelStyle, { color: props.isWhite ? Colors.white : Colors.black }, props.titleStyle]} >{props.title}</Text>
                 }
             </View>
             <View style={{ flex: 1, alignItems: "flex-end", justifyContent: "center" }} >
