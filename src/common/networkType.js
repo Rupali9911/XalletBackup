d029d2b1d3f1ace1657cfb37aecf1486cbf067bb
shubham-noborders <shubham.kothari@noborderz.com>
2022-03-29 19:31:09 +0530

    Merged api call changes

--- src/common/networkType.js
+++ src/common/networkType.js
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
-//export const networkType = "mainnet";
-=======
 // export const networkType = "mainnet";
->>>>>>> 1857c7e1f44ec9b46472b523842c4de977af3251
 export const networkType = "testnet";
