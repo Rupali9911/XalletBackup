3e8f9ea7125a7e2fd70ac70b28c88935936457e4
harisNB <haris@noborderz.com>
2022-04-03 22:05:34 -0500

    code backup

--- src/screens/explore/index.js
+++ src/screens/explore/index.js
@@ -5,6 +5,7 @@ import { Container } from 'src/styles/common.styles';
 import { Loader } from '../../components';
 import AppSearch from '../../components/appSearch';
 import { hp } from '../../constants/responsiveFunct';
+import { BASE_URL } from '../../helpers/ApiRequest';
 import { colors } from '../../res';
 import { getNFTList, nftListReset, nftLoadStart, pageChange } from '../../store/actions/nftTrendList';
 import NftItem from '../detailScreen/nftItem';
@@ -12,18 +13,43 @@ import NftItem from '../detailScreen/nftItem';
 function ExploreScreen() {
   const { ListReducer } = useSelector(state => state);
   const [nftIndex, setNftIndex] = useState(2);
+  const [page, setPage] = useState(1);
+  const [count, setCount] = useState(0);
+  const [discoverNFTList, setDiscoverNFTList] = useState([]);
   const [isFirsRender, setIsFirstRender] = useState(true);
 
   useEffect(() => {
-    if (ListReducer?.nftList?.length == 0 && isFirsRender) {
-      dispatch(nftLoadStart());
-      dispatch(nftListReset('hot'));
-      getNFTlistData(1);
-      dispatch(pageChange(1));
-      setIsFirstRender(false)
-    } else {
-      setIsFirstRender(false)
-    }
+    // let url = `${BASE_URL}/xanalia/discover?page=${page}&limit=18`;
+    let url = "https://testapi.xanalia.com/xanalia/discover?page=1&limit=18";
+    let fetch_data_body = {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    };
+
+    fetch(url, fetch_data_body)
+      .then(response => response.json())
+      .then(json => {
+        console.log('json discover', json)
+        if(json.success){
+          setDiscoverNFTList(json.data)
+          setCount(json.count)
+        }
+      })
+      .catch(err => {
+        console.log(err, "error discover")
+      });
+    // if (ListReducer?.nftList?.length == 0 && isFirsRender) {
+    //   dispatch(nftLoadStart());
+    //   dispatch(nftListReset('hot'));
+    //   getNFTlistData(1);
+    //   dispatch(pageChange(1));
+    //   setIsFirstRender(false)
+    // } else {
+    //   setIsFirstRender(false)
+    // }
   }, [ListReducer?.nftList]);
 
   const dispatch = useDispatch();
@@ -32,9 +58,9 @@ function ExploreScreen() {
     dispatch(getNFTList(page));
   }, []);
 
-  const handlePageChange = page => {
-    dispatch(pageChange(page));
-  };
+  // const handlePageChange = page => {
+  //   dispatch(pageChange(page));
+  // };
 
   const renderFooter = () => {
     if (!ListReducer.nftListLoading) return null;
@@ -49,12 +75,12 @@ function ExploreScreen() {
   };
 
   const memoizedValue = useMemo(() => renderItem, [ListReducer.nftList]);
-
+  console.log(ListReducer?.nftList, "ListReducer?.nftList")
   return (
     <Container>
       <View style={{ flex: 1 }}>
         <View style={styles.listContainer}>
-          {isFirsRender ? isFirsRender : ListReducer.page === 1 && ListReducer.nftListLoading ? (
+          {/* {isFirsRender ? isFirsRender : ListReducer.page === 1 && ListReducer.nftListLoading ? (
             <View style={styles.loaderContainer}>
               <Loader />
             </View>
@@ -76,7 +102,27 @@ function ExploreScreen() {
               onEndReachedThreshold={0.1}
               keyExtractor={(v, i) => 'item_' + i}
             />
-          )}
+          )} */}
+          {
+            discoverNFTList.length !== 0 &&
+            <FlatList
+                data={discoverNFTList}
+                renderItem={memoizedValue}
+                onEndReached={() => {
+                  // let tempIndex = nftIndex + 2;
+                  // setNftIndex(tempIndex)
+  
+                  // if (ListReducer?.nftList?.length == tempIndex && !ListReducer.nftListLoading) {
+                  //   let num = ListReducer.page + 1;
+                  //   getNFTlistData(num);
+                  //   handlePageChange(num);
+                  // }
+                }}
+                ListFooterComponent={renderFooter}
+                onEndReachedThreshold={0.1}
+                keyExtractor={(v, i) => 'item_' + i}
+              />
+          }
         </View>
         <AppSearch />
       </View>
