9e5196cfe33843b7174c0e85f40a86dffb7487ea
dinesh111993 <dinesh.patidar@noborderz.com>
2022-05-04 13:02:02 +0530

    first commit

--- src/components/CollectionItem/index.js
+++ src/components/CollectionItem/index.js
@@ -1,63 +1,87 @@
 import React from 'react';
-import { TouchableOpacity, View, Text, Image, Platform } from 'react-native';
-import { C_Image } from '../../components';
+import {TouchableOpacity, View, Text, Image, Platform} from 'react-native';
+import {C_Image} from '../../components';
 import styles from './styles';
-import { SIZE, SVGS } from 'src/constants';
-import { translate } from '../../walletUtils';
+import {SIZE, SVGS} from 'src/constants';
+import {translate} from '../../walletUtils';
 
-
-const {
-  PolygonIcon,
-  Ethereum,
-  BitmapIcon,
-} = SVGS;
+const {PolygonIcon, Ethereum, BitmapIcon} = SVGS;
 
 export default function CollectionItem(props) {
-  const { bannerImage, chainType, items, iconImage, collectionName, creatorInfo, onPress, creator, blind, isCollection } = props;
+  const {
+    bannerImage,
+    chainType,
+    items,
+    iconImage,
+    collectionName,
+    creatorInfo,
+    onPress,
+    creator,
+    blind,
+    isCollection,
+  } = props;
 
-  const chainIcon = (type) => {
-    if (type === 'polygon') return <PolygonIcon />
-    if (type === 'ethereum') return <Ethereum />
-    if (type === 'binance') return <BitmapIcon />
+  const chainIcon = type => {
+    if (type === 'polygon') {
+      return <PolygonIcon />;
+    }
+    if (type === 'ethereum') {
+      return <Ethereum />;
+    }
+    if (type === 'binance') {
+      return <BitmapIcon />;
+    }
   };
 
   const getByUser = () => {
-    if (creator) return creator;
-    if (creatorInfo[0].title) return creatorInfo[0].title;
+    if (creator) {
+      return creator;
+    }
+    if (creatorInfo[0].title) {
+      return creatorInfo[0].title;
+    }
     if (creatorInfo[0].role === 'crypto') {
       return creatorInfo[0].username.slice(0, 6);
     } else {
       return creatorInfo[0].username;
     }
-  }
+  };
 
-  let uriType = bannerImage?.split('.')[bannerImage?.split('.').length - 1]
-  const checkVideoUrl = uriType === 'mp4' || uriType === 'MP4' || uriType === 'mov' || uriType === 'MOV';
+  let uriType = bannerImage?.split('.')[bannerImage?.split('.').length - 1];
+  const checkVideoUrl =
+    uriType === 'mp4' ||
+    uriType === 'MP4' ||
+    uriType === 'mov' ||
+    uriType === 'MOV';
 
   const renderChain = () => {
     if (blind) {
       return (
-        <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
-          <BitmapIcon style={{ marginRight: SIZE(8)}} />
-          <PolygonIcon style={{ marginRight: SIZE(8)}} />
+        <View style={{flexDirection: 'row', justifyContent: 'center'}}>
+          <BitmapIcon style={{marginRight: SIZE(8)}} />
+          <PolygonIcon style={{marginRight: SIZE(8)}} />
           <Ethereum />
         </View>
       );
     } else {
       return chainIcon(chainType);
     }
-  }
+  };
 
   return (
-    <TouchableOpacity
-      onPress={onPress}
-      style={styles.collectionListItem}>
+    <TouchableOpacity onPress={onPress} style={styles.collectionListItem}>
       <View style={styles.listItemContainer}>
         <View>
           <C_Image
             type={uriType}
             uri={bannerImage}
-            imageStyle={Platform.OS === "ios" ? (checkVideoUrl ? styles.collectionListVideo : styles.collectionListImage) : styles.collectionListImage}
+            imageStyle={
+              Platform.OS === 'ios'
+                ? checkVideoUrl
+                  ? styles.collectionListVideo
+                  : styles.collectionListImage
+                : styles.collectionListImage
+            }
           />
         </View>
         <View style={styles.collectionWrapper}>
@@ -67,17 +91,21 @@ export default function CollectionItem(props) {
             imageStyle={styles.iconImage}
           />
           <View style={styles.bottomCenterWrap}>
-            <Text numberOfLines={1} style={styles.collectionName}>{collectionName}</Text>
-            {!isCollection && <Text style={styles.byUser}>{`by ${getByUser()}`}</Text>}
+            <Text numberOfLines={1} style={styles.collectionName}>
+              {collectionName}
+            </Text>
+            {!isCollection && (
+              <Text style={styles.byUser}>{`by ${getByUser()}`}</Text>
+            )}
           </View>
           <View style={styles.bottomWrap}>
             {!isCollection ? renderChain() : <View />}
-            <Text style={{ fontSize: SIZE(12), color: '#8e9bba' }}>
+            <Text style={{fontSize: SIZE(12), color: '#8e9bba'}}>
               {`${items} ` + translate('common.itemsCollection')}
             </Text>
           </View>
         </View>
       </View>
     </TouchableOpacity>
-  )
+  );
 }
