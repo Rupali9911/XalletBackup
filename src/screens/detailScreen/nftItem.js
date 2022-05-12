e1f2fe752e4b30808584716739b887a352a5f705
shubham-noborders <shubham.kothari@noborderz.com>
2022-05-12 18:43:12 +0530

    Reversed changes from Pratik

--- src/screens/detailScreen/nftItem.js
+++ src/screens/detailScreen/nftItem.js
@@ -547,7 +547,7 @@ const nftItem = ({ item, index, minHeight, screenName }) => {
       lines.length >= 2 &&
       lines[1].width > width - SIZE(40)
     )
-      setLengthMore(true);   
+      setLengthMore(true);
   }, []);
 
   const onProfile = isOwner => {
@@ -565,27 +565,74 @@ const nftItem = ({ item, index, minHeight, screenName }) => {
   const imageUri = item?.thumbnailUrl;
 
   const image = item.metaData.image || item.thumbnailUrl;
-  const fileType = image ? image?.split('.')[image?.split('.').length - 1] : '';
+    const fileType = videoUri ? videoUri?.split('.')[videoUri?.split('.').length - 1] : '';
 
   let disableCreator = false;
 
-  let artistName = artistData && artist
-    ? artist.includes("0x")
-      ? artistData.hasOwnProperty("title") && artistData.title ?
-        artistData.title
-        : (artist === '0x913d90bf7e4A2B1Ae54Bd5179cDE2e7cE712214A'.toLowerCase()
-          || artist === '0xf45C0d38Df3eac6bf6d0fF74D53421Dc34E14C04'.toLowerCase()
-          || artist === '0x77FFb287573b46AbDdcEB7F2822588A847358933'.toLowerCase())
-          ? (
-            disableCreator = true,
-            collectCreat?.creator
-          )
-          : artist.substring(0, 6)
-      : artistData === "No record found" ?
-        artist.substring(0, 6) :
-        artistData.hasOwnProperty("username") && artistData.username ?
-          artistData.username.substring(0, 6) : artist.substring(0, 6)
-    : artist.substring(0, 6)
+    let artistName = artistData && typeof artistData === 'object' ?
+        artistData?.role === 'crypto' ?
+            artistData?.title?.trim() ? artistData.title :
+                artistData?.name?.trim() ? artistData.name :
+                    artistData?.username?.trim() ? artistData.username.substring(0, 6) :
+                        (artist === '0x913d90bf7e4A2B1Ae54Bd5179cDE2e7cE712214A'.toLowerCase()
+                            || artist === '0xf45C0d38Df3eac6bf6d0fF74D53421Dc34E14C04'.toLowerCase()
+                            || artist === '0x77FFb287573b46AbDdcEB7F2822588A847358933'.toLowerCase()
+                            || artist === '0xfaae9d5b6f4779689bd273ab30f78beab3a0fc8f'.toLowerCase())
+                            ? (
+                                disableCreator = true,
+                                    collectCreatData?.creator
+                            ) : artist ? artist?.substring(0, 6) : ""
+            : artistData?.username?.trim() ? artistData.username :
+            artistData?.name?.trim() ? artistData.name :
+                artistData?.title?.trim() ? artistData.title : artist ? artist?.substring(0, 6) : ""
+        : artist ? artist?.substring(0, 6) : ""
+
+    // console.log("🚀 ~ file: nftItem.js ~ line 571", artistData, '</>', artist, '>>>>')
+    // console.log("🚀 ~ file: nftItem.js ~ line 572 ~ nftItem ~ artistName", artistName)
+
+    let ownerName = ownerData && typeof ownerData === 'object' ?
+        ownerData?.role === 'crypto' ?
+            ownerData?.title?.trim() ? ownerData.title :
+                ownerData?.name?.trim() ? ownerData.name :
+                    ownerData?.username?.trim() ? ownerData.username.substring(0, 6) : owner ? owner.substring(0, 6) : ""
+            : ownerData?.username?.trim() ? ownerData.username :
+            ownerData?.name?.trim() ? ownerData.name :
+                ownerData?.title?.trim() ? ownerData.title : owner ? owner.substring(0, 6) : ""
+        : owner ? owner.substring(0, 6) : ""
+
+    // console.log("🚀 ~ file: nftItem.js ~ line 573", ownerData, '</>', owner, '>>>>')
+    // console.log("🚀 ~ file: nftItem.js ~ line 574 ~ nftItem ~ ownerName", ownerName)
+
+    // let ownerName = ownerData ? (
+    //   ownerData.role === 'crypto' ?
+    //     ownerData.title ?
+    //       ownerData.title :
+    //       owner.includes("0x")
+    //         ? owner.substring(0, 6)
+    //         : owner.substring(0, 6) :
+    //     ownerData.role === 'non_crypto' ?
+    //       ownerData.username ?
+    //         ownerData.username : ""
+    //       : owner) :
+    //   owner
+
+    // let artistName = artistData && artist
+    //   ? artist.includes("0x")
+    //     ? artistData.hasOwnProperty("title") && artistData.title ?
+    //       artistData.title
+    //       : (artist === '0x913d90bf7e4A2B1Ae54Bd5179cDE2e7cE712214A'.toLowerCase()
+    //         || artist === '0xf45C0d38Df3eac6bf6d0fF74D53421Dc34E14C04'.toLowerCase()
+    //         || artist === '0x77FFb287573b46AbDdcEB7F2822588A847358933'.toLowerCase())
+    //         ? (
+    //           disableCreator = true,
+    //           collectCreat?.creator
+    //         )
+    //         : artist.substring(0, 6)
+    //     : artistData === "No record found" ?
+    //       artist.substring(0, 6) :
+    //       artistData.hasOwnProperty("username") && artistData.username ?
+    //         artistData.username.substring(0, 6) : artist.substring(0, 6)
+    //   : artist.substring(0, 6)
 
   return (
     <>
@@ -641,20 +688,7 @@ const nftItem = ({ item, index, minHeight, screenName }) => {
                   <Text
                     numberOfLines={1}
                     style={[styles.iconLabel, { maxWidth: width * 0.35 }]}>
-                    {
-                      ownerData ? (
-                        ownerData.role === 'crypto' ?
-                          ownerData.title ?
-                            ownerData.title :
-                            owner.includes("0x")
-                              ? owner.substring(0, 6)
-                              : owner.substring(0, 6) :
-                          ownerData.role === 'non_crypto' ?
-                            ownerData.username ?
-                              ownerData.username : ""
-                            : owner) :
-                        owner
-                    }
+                    {ownerName}
                   </Text>
                 </View>
               </TouchableOpacity>
@@ -684,7 +718,7 @@ const nftItem = ({ item, index, minHeight, screenName }) => {
                 }}>
                 {fileType === 'mp4' ||
                   fileType === 'MP4' ||
-                  fileType === 'mov' ||
+                fileType === 'mov' || fileType === 'movie' ||
                   fileType === 'MOV' ? (
                   <View style={styles.modalImage}>
                     <Video
