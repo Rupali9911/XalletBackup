99157fcd93725a1d2f2a5d8a0a01d139d50ea0f8
rupali-noborderz <rupali.chouhan@noborderz.com>
2022-11-15 12:50:41 +0530

    JP-AI chat keyboard issue - IOS

--- src/screens/AiChat/MessageInput.js
+++ src/screens/AiChat/MessageInput.js
@@ -1,35 +1,35 @@
 import {
-    View,
-    Text,
-    TouchableOpacity,
-    TextInput,
-  } from 'react-native';
-  import styles from './style';
-  import React, { useState, useEffect } from 'react';
-  import { translate } from '../../walletUtils';
-  
-  const MessageInput = (props) => {
-  
-    //=====================(Main return Function)=============================
-    return (
-      <View>
-        <View style={[styles.separator, { marginVertical: 5 }]} />
-        <View style={styles.inputContainer}>
-          <TextInput
-            value={props.value}
-            placeholder={props.placeholder}
-            onChangeText={props.onChangeText}
-            style={styles.input}
-            placeholderTextColor={'#212529'}
-            selectionColor={'#212529'}
-            
-          />
-          <TouchableOpacity style={styles.sendBtn} onPress={props.onPress}>
-            <Text style={styles.sendBtnTxt}>{translate("wallet.common.send")}</Text>
-          </TouchableOpacity>
-        </View>
+  View,
+  Text,
+  TouchableOpacity,
+  TextInput,
+} from 'react-native';
+import styles from './style';
+import React, { useState, useEffect } from 'react';
+import { translate } from '../../walletUtils';
+import { Platform } from 'expo-modules-core';
+
+const MessageInput = (props) => {
+
+  //=====================(Main return Function)=============================
+  return (
+    <View>
+      <View style={[styles.separator, {marginVertical: Platform.OS === 'ios' ? 5 : 0 }]} />
+      <View style={styles.inputContainer}>
+        <TextInput
+          value={props.value}
+          placeholder={props.placeholder}
+          onChangeText={props.onChangeText}
+          style={styles.input}
+          placeholderTextColor={'#212529'}
+          selectionColor={'#212529'}
+        />
+        <TouchableOpacity style={styles.sendBtn} onPress={props.onPress}>
+          <Text style={styles.sendBtnTxt}>{translate("wallet.common.send")}</Text>
+        </TouchableOpacity>
       </View>
-    );
-  };
-  
-  export default MessageInput;
+    </View>
+  );
+};
+
+export default MessageInput;
