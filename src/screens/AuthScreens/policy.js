34680aaf1461fac8c7d7c0085ca53b33b2cb2764
Apple <apple@Apples-MacBook-Pro-2.local>
2022-11-28 17:25:32 +0530

    Released version 2.12.09 Testnet and mainnet builds

--- src/screens/AuthScreens/policy.js
+++ src/screens/AuthScreens/policy.js
@@ -1,5 +1,5 @@
-import React, {useEffect, useRef, useState} from 'react';
-import {StyleSheet, Platform, Linking, BackHandler} from 'react-native';
+import React, {useRef, useState} from 'react';
+import {StyleSheet, Platform, Linking} from 'react-native';
 import AppBackground from '../../components/appBackground';
 import WebView from 'react-native-webview';
 import AppHeader from '../../components/appHeader';
@@ -9,52 +9,33 @@ const terms = require('../../walletUtils/terms.html');
 
 const Policy = ({route}) => {
   const {isPolicy} = route.params;
-  let count = 1;
 
   const [loading, setLoading] = useState(true);
 
   let contentIos = isPolicy ? policy : terms;
 
-  let contentAn = isPolicy
-    ? 'file:///android_asset/policy.html'
-    : 'file:///android_asset/terms.html';
+  let contentAn = isPolicy ? 'file:///android_asset/policy.html' : 'file:///android_asset/terms.html';
 
-  var webViewObj = {
+  var  webViewObj = {
     canGoBack: false,
     ref: null,
-  };
-  const onMessage = event => {
-    console.log('Meesaage from Webview', event);
-    Linking.openURL(event.nativeEvent.data);
-  };
 
-  var webViewObj = {
-    canGoBack: false,
-    ref: null,
-  };
+}
+  const onMessage= (event) => {
+    console.log('Meesaage from Webview',event) 
+     Linking.openURL(event.nativeEvent.data)
+  }
 
-  const handleWebViewNavigationStateChange = navState => {
-    console.log('Navstate', navState);
+  const handleWebViewNavigationStateChange=(navState)=>{
+    console.log('Navstate', navState)
 
-    if (Platform.OS === 'ios') {
-      if (count > 0) {
-        count = 0;
-      } else {
         if (navState.url.indexOf('xanalia.com') > 0) {
-          webViewObj.canGoBack = navState.canGoBack;
-          webViewObj.ref.goBack();
-          Linking.openURL(navState.url);
-          return false;
+          setLoading(false)
+          webViewObj.ref.stopLoading()
+             Linking.openURL(navState.url);
+            return false
         }
-      }
-    } else {
-      if (navState.url.indexOf('xanalia.com') > 0) {
-        webViewObj.ref.goBack();
-        Linking.openURL(navState.url);
-        return false;
-      }
-    }
-  };
+  }
 
   return (
     <AppBackground isBusy={loading}>
@@ -70,35 +51,22 @@ const Policy = ({route}) => {
           domStorageEnabled={true}
           onLoadStart={() => setLoading(true)}
           onLoadEnd={() => setLoading(false)}
-          onNavigationStateChange={navState => {
-            handleWebViewNavigationStateChange(navState);
-          }}
-          ref={webView => {
-            webViewObj.ref = webView;
-          }}
-          allowsBackForwardNavigationGestures={true}
-          androidHardwareAccelerationDisabled={true}
-          renderToHardwareTextureAndroid={true}
+          onNavigationStateChange={(navState) => {handleWebViewNavigationStateChange(navState)}}
+          ref={(webView) => { webViewObj.ref = webView; }}
         />
       ) : (
         <WebView
           style={styles.webview}
           originWhitelist={['*']}
           source={contentIos}
-          onMessage={event => onMessage(event)}
+          onMessage={event => onMessage(event) }
           decelerationRate="normal"
           javaScriptEnabled={true}
           domStorageEnabled={true}
           onLoadStart={() => setLoading(true)}
           onLoadEnd={() => setLoading(false)}
-          onNavigationStateChange={navState => {
-            handleWebViewNavigationStateChange(navState);
-          }}
-          bounces={false}
-          loading
-          ref={webView => {
-            webViewObj.ref = webView;
-          }}
+          onNavigationStateChange={(navState) => {handleWebViewNavigationStateChange(navState)}}
+          ref={(webView) => { webViewObj.ref = webView; }}
         />
       )}
     </AppBackground>
