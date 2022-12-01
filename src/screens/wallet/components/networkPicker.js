8f77045fa7384bc8b1dc3b5767d3bd306232f8d7
rupali11NB <rupali.chouhan@noborderz.com>
2022-12-01 11:22:42 +0530

    Splash change for Android

--- src/screens/wallet/components/networkPicker.js
+++ src/screens/wallet/components/networkPicker.js
@@ -1,80 +1,87 @@
 import React from 'react';
 import {
-    Modal,
-    View,
-    TouchableOpacity,
-    StyleSheet,
-    SafeAreaView,
-    FlatList,
-    Image
+  Modal,
+  View,
+  TouchableOpacity,
+  StyleSheet,
+  SafeAreaView,
+  FlatList,
+  Image,
 } from 'react-native';
 import Colors from '../../../constants/Colors';
-import { startLoader } from '../../../store/reducer/userReducer';
-import { wp, hp, RF } from '../../../constants/responsiveFunct';
+import {startLoader} from '../../../store/reducer/userReducer';
+import {wp, hp, RF} from '../../../constants/responsiveFunct';
 import ImagesSrc from '../../../constants/Images';
 import TextView from '../../../components/appText';
 import Checkbox from '../../../components/checkbox';
-import { networkChain } from '../../../walletUtils';
-import { useSelector } from "react-redux";
+import {networkChain} from '../../../walletUtils';
+import {useSelector} from 'react-redux';
 
-const NetworkPicker = (props) => {
+const NetworkPicker = props => {
+  const {visible, onRequestClose, network, onItemSelect} = props;
+  const {selectedLanguageItem} = useSelector(state => state.LanguageReducer);
+  const {networks} = useSelector(state => state.NetworkReducer);
 
-    const { visible, onRequestClose, network, onItemSelect } = props;
-    const { selectedLanguageItem } = useSelector(state => state.LanguageReducer);
-    const { networks } = useSelector(state => state.NetworkReducer);
-
-    return (
-        <Modal
-            visible={visible}
-            transparent
-            onRequestClose={onRequestClose}>
-            <View style={styles.container}>
-                <TouchableOpacity style={styles.emptyView} onPress={onRequestClose} />
-                <View style={styles.contentContainer}>
-                    <FlatList
-                        data={networks}
-                        keyExtractor={(item, index) => `_${index}`}
-                        renderItem={({ item, index }) => {
-                            const isCheck = network ? network.name === item.name : false;
-                            return <TouchableOpacity style={styles.listItem} onPress={() => onItemSelect(item)}>
-                                {/* <TextView style={styles.text}>{selectedLanguageItem.language_name === 'ja' ? item.translatedName : item.name}</TextView> */}
-                                <TextView style={styles.text}>{item.name}</TextView>
-                                <Image style={[styles.logoSize, props.logoStyle]} resizeMode="contain" source={isCheck ? ImagesSrc.checkIcon : ImagesSrc.unCheckIcon} />
-                            </TouchableOpacity>
-                        }} />
-                </View>
-            </View>
-        </Modal>
-    );
-}
+  return (
+    <Modal visible={visible} transparent onRequestClose={onRequestClose}>
+      <View style={styles.container}>
+        <TouchableOpacity style={styles.emptyView} onPress={onRequestClose} />
+        <View style={styles.contentContainer}>
+          <FlatList
+            data={networks}
+            keyExtractor={(item, index) => `_${index}`}
+            renderItem={({item, index}) => {
+              const isCheck = network ? network.name === item.name : false;
+              return (
+                <TouchableOpacity
+                  style={styles.listItem}
+                  onPress={() => onItemSelect(item)}>
+                  {/* <TextView style={styles.text}>{selectedLanguageItem.language_name === 'ja' ? item.translatedName : item.name}</TextView> */}
+                  <TextView style={styles.text}>{item.name}</TextView>
+                  <Image
+                    style={[styles.logoSize, props.logoStyle]}
+                    resizeMode="contain"
+                    source={
+                      isCheck ? ImagesSrc.checkIcon : ImagesSrc.unCheckIcon
+                    }
+                  />
+                </TouchableOpacity>
+              );
+            }}
+          />
+        </View>
+      </View>
+    </Modal>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: Colors.transparentModal
-    },
-    emptyView: {
-        flex: 1,
-    },
-    contentContainer: {
-        backgroundColor: Colors.white,
-        padding: wp("4%"),
-        paddingBottom: hp("4%")
-    },
-    listItem: {
-        flexDirection: 'row',
-        paddingVertical: hp("1%"),
-        alignItems: 'center'
-    },
-    text: {
-        flex: 1,
-        fontSize: RF(2.3),
-        color: Colors.black,
-    },
-    logoSize: {
-        width: wp("7.5%"),
-        height: wp("7.5%"),
-    }
+  container: {
+    flex: 1,
+    backgroundColor: Colors.transparentModal,
+  },
+  emptyView: {
+    flex: 1,
+  },
+  contentContainer: {
+    backgroundColor: Colors.white,
+    padding: wp('4%'),
+    paddingBottom: hp('4%'),
+  },
+  listItem: {
+    flexDirection: 'row',
+    paddingVertical: hp('1%'),
+    alignItems: 'center',
+  },
+  text: {
+    flex: 1,
+    fontSize: RF(2.3),
+    color: Colors.black,
+  },
+  logoSize: {
+    width: wp('7.5%'),
+    height: wp('7.5%'),
+  },
 });
 
 export default NetworkPicker;
