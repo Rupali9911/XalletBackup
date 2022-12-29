b90d6c25b12439e2536c3bd7024a932eb029f12c
Pratik Modi <pratik.modi@noborderz.com>
2022-12-29 14:16:19 +0530

    Fix: Modal issues

--- src/screens/wallet/components/networkPicker.js
+++ src/screens/wallet/components/networkPicker.js
@@ -7,10 +7,17 @@ import {
   SafeAreaView,
   FlatList,
   Image,
+  Platform,
 } from 'react-native';
 import Colors from '../../../constants/Colors';
 import {startLoader} from '../../../store/reducer/userReducer';
-import {wp, hp, RF} from '../../../constants/responsiveFunct';
+import {
+  wp,
+  hp,
+  RF,
+  screenHeight,
+  screenWidth,
+} from '../../../constants/responsiveFunct';
 import ImagesSrc from '../../../constants/Images';
 import TextView from '../../../components/appText';
 import Checkbox from '../../../components/checkbox';
@@ -22,45 +29,95 @@ const NetworkPicker = props => {
   const {selectedLanguageItem} = useSelector(state => state.LanguageReducer);
   const {networks} = useSelector(state => state.NetworkReducer);
 
-  return (
-    <Modal visible={visible} transparent onRequestClose={onRequestClose}>
-      <View style={styles.container}>
-        <TouchableOpacity style={styles.emptyView} onPress={onRequestClose} />
-        <View style={styles.contentContainer}>
-          <FlatList
-            data={networks}
-            keyExtractor={(item, index) => `_${index}`}
-            renderItem={({item, index}) => {
-              const isCheck = network ? network.name === item.name : false;
-              return (
-                <TouchableOpacity
-                  style={styles.listItem}
-                  onPress={() => onItemSelect(item)}>
-                  {/* <TextView style={styles.text}>{selectedLanguageItem.language_name === 'ja' ? item.translatedName : item.name}</TextView> */}
-                  <TextView style={styles.text}>
-                    {item.name === 'XANACHAIN' ? 'XANAChain' : item.name}
-                  </TextView>
-                  <Image
-                    style={[styles.logoSize, props.logoStyle]}
-                    resizeMode="contain"
-                    source={
-                      isCheck ? ImagesSrc.checkIcon : ImagesSrc.unCheckIcon
-                    }
-                  />
-                </TouchableOpacity>
-              );
-            }}
-          />
+  if (Platform.OS !== 'android') {
+    return (
+      <Modal visible={visible} transparent onRequestClose={onRequestClose}>
+        <View style={styles.container}>
+          <TouchableOpacity style={styles.emptyView} onPress={onRequestClose} />
+          <View style={styles.contentContainer}>
+            <FlatList
+              data={networks}
+              keyExtractor={(item, index) => `_${index}`}
+              renderItem={({item, index}) => {
+                const isCheck = network ? network.name === item.name : false;
+                return (
+                  <TouchableOpacity
+                    style={styles.listItem}
+                    onPress={() => onItemSelect(item)}>
+                    {/* <TextView style={styles.text}>{selectedLanguageItem.language_name === 'ja' ? item.translatedName : item.name}</TextView> */}
+                    <TextView style={styles.text}>
+                      {item.name === 'XANACHAIN' ? 'XANAChain' : item.name}
+                    </TextView>
+                    <Image
+                      style={[styles.logoSize, props.logoStyle]}
+                      resizeMode="contain"
+                      source={
+                        isCheck ? ImagesSrc.checkIcon : ImagesSrc.unCheckIcon
+                      }
+                    />
+                  </TouchableOpacity>
+                );
+              }}
+            />
+          </View>
         </View>
-      </View>
-    </Modal>
-  );
+      </Modal>
+    );
+  } else {
+    if (visible) {
+      return (
+        <TouchableOpacity activeOpacity={1} style={styles.andcontainer}>
+          <TouchableOpacity
+            style={styles.emptyView}
+            activeOpacity={1}
+            onPress={() => onRequestClose()}
+          />
+          <View style={styles.contentContainer}>
+            <FlatList
+              data={networks}
+              keyExtractor={(item, index) => `_${index}`}
+              renderItem={({item, index}) => {
+                const isCheck = network ? network.name === item.name : false;
+                return (
+                  <TouchableOpacity
+                    style={styles.listItem}
+                    onPress={() => onItemSelect(item)}>
+                    {/* <TextView style={styles.text}>{selectedLanguageItem.language_name === 'ja' ? item.translatedName : item.name}</TextView> */}
+                    <TextView style={styles.text}>
+                      {item.name === 'XANACHAIN' ? 'XANAChain' : item.name}
+                    </TextView>
+                    <Image
+                      style={[styles.logoSize, props.logoStyle]}
+                      resizeMode="contain"
+                      source={
+                        isCheck ? ImagesSrc.checkIcon : ImagesSrc.unCheckIcon
+                      }
+                    />
+                  </TouchableOpacity>
+                );
+              }}
+            />
+          </View>
+        </TouchableOpacity>
+      );
+    } else {
+      return null;
+    }
+  }
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: Colors.transparentModal,
+  },
+  andcontainer: {
+    position: 'absolute',
+    top: 0,
+    zIndex: 1,
+    backgroundColor: Colors.blackOpacity(0.3),
+    width: screenWidth,
+    height: screenHeight - 50,
   },
   emptyView: {
     flex: 1,
