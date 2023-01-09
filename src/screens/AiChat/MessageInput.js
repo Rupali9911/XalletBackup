5a4bd5eb763665919d7e6a34f144cd72ea1321a1
Raj-Noborderz <rajkumar@noborderz.com>
2023-01-09 16:20:26 +0530

    Chat detail re-rendering & Mobile-507 collection issue fixed

--- src/screens/AiChat/MessageInput.js
+++ src/screens/AiChat/MessageInput.js
@@ -2,23 +2,35 @@ import {View, Text, TouchableOpacity, TextInput, Platform} from 'react-native';
 import styles from './style';
 import React, {useState, useEffect} from 'react';
 import {translate} from '../../walletUtils';
+import {colors} from '../../res';
 
 const MessageInput = props => {
+  const [message, setMessage] = useState('');
+
   //=====================(Main return Function)=============================
   return (
     <View>
       <View style={styles.separator} />
       <View style={styles.inputContainer}>
         <TextInput
-          value={props.value}
-          placeholder={props.placeholder}
-          onChangeText={props.onChangeText}
+          value={message}
+          placeholder={
+            props?.placeholder
+              ? props.placeholder
+              : translate('common.enterMessage')
+          }
+          onChangeText={text => setMessage(text)}
           style={styles.input}
-          placeholderTextColor={'#212529'}
-          selectionColor={'#212529'}
+          placeholderTextColor={colors.BLACK7}
+          selectionColor={colors.BLACK7}
           onFocus={props.onFocus}
         />
-        <TouchableOpacity style={styles.sendBtn} onPress={props.onPress}>
+        <TouchableOpacity
+          style={styles.sendBtn}
+          onPress={() => {
+            props.onPress(message);
+            setMessage('');
+          }}>
           <Text style={styles.sendBtnTxt}>
             {translate('wallet.common.send')}
           </Text>
