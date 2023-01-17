9964061f78d095864d00345606eaf01e297785c2
rupali11NB <rupali.chouhan@noborderz.com>
2023-01-17 18:13:34 +0530

    Mobile 492

--- src/screens/profile/index.js
+++ src/screens/profile/index.js
@@ -1,4 +1,5 @@
 import React, {useCallback, useEffect, useMemo, useRef, useState} from 'react';
+import {useIsFocused} from '@react-navigation/native';
 import {
   Linking,
   StyleSheet,
@@ -72,6 +73,7 @@ function Profile({navigation, connector, route}) {
   const scrollRef = useRef(null);
   const dispatch = useDispatch();
   const ref = React.useRef();
+  const isFocused = useIsFocused();
 
   // =============== Getting data from reducer ========================
   const {
@@ -663,7 +665,6 @@ function Profile({navigation, connector, route}) {
     [],
   );
 
-  // console.log('This is ID : ', id);
   return (
     <AppBackground>
       {/* <ScrollView
@@ -695,18 +696,18 @@ function Profile({navigation, connector, route}) {
       <Tabs.Container
         renderHeader={RenderHeader}
         lazy={true}
+        cancelLazyFadeIn={true}
+        renderTabBar={TabBarComponent}
         onIndexChange={index => {
           setCurrentTabIndex(index);
-        }}
-        renderTabBar={TabBarComponent}>
+        }}>
         <Tabs.Tab
           name={translate('wallet.common.profileCreated')}
           key={'profileCreated'}>
-          <NFTCreated id={id} />
+          <NFTCreated id={id} isFocused={isFocused} />
         </Tabs.Tab>
-
         <Tabs.Tab name={translate('wallet.common.owned')} key={'nftOwned'}>
-          <NFTOwned id={id} />
+          <NFTOwned id={id} isFocused={isFocused} />
         </Tabs.Tab>
       </Tabs.Container>
     </AppBackground>
